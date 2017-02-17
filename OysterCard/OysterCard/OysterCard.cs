9de62a99194a2d32573806b72b4c63390ed202bf
alexanderwjrussell <awjrussell@live.co.uk>
2017-02-17 16:29:23 +0000

    OysterCard Tests 1) Limiting maximum balance on the card. Test passing 2) Ability to deduct funds from the card. Test passing

--- OysterCard/OysterCard/OysterCard.cs
+++ OysterCard/OysterCard/OysterCard.cs
@@ -1,8 +1,11 @@
-﻿namespace OysterCard
+﻿using System;
+
+namespace OysterCard
 {
     public class OysterCard
     {
         public int Balance { get; private set; }
+        public const int MAXIMUM_BALANCE = 100;
 
         public OysterCard()
         {
@@ -11,7 +14,14 @@
 
         public void TopUp(int amount)
         {
+            if ( Balance + amount > MAXIMUM_BALANCE)
+                throw new Exception("Trying to top up more than maximum balance");
             Balance += amount;
+        }
+
+        public void Deduct(int amount)
+        {
+            Balance -= amount;
         }
     }
 }
--- OysterCard/OysterCardTests/OysterCardTests.cs
+++ OysterCard/OysterCardTests/OysterCardTests.cs
@@ -23,5 +23,24 @@ namespace OysterCard
 
             Assert.That(oysterCard.Balance, Is.EqualTo(10));
         }
+
+        [Test]
+        public void Should_NotBeAbleTo_PutMoreMoneyOnTheCardThanMaximumBalance()
+        {
+            var oysterCard = new OysterCard();
+
+            Assert.Throws<Exception>(() => oysterCard.TopUp(1000));
+        }
+
+        [Test]
+        public void Should_BeAbleTo_DeductMoneyForFare()
+        {
+            var oysterCard = new OysterCard();
+
+            oysterCard.TopUp(20);
+            oysterCard.Deduct(10);
+
+            Assert.That(oysterCard.Balance, Is.EqualTo(10));
+        }
     }
 }
